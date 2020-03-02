204b94d167cbb2c32b17fb3c0dd85d3db8c85774
andrey <andreyzasulevichdev@gmail.com>
2020-03-02 18:57:28 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var bracketsLength = bracketsConfig.length;
+
+    for (var i = 0; i < bracketsLength; ) {
+        var brackets = bracketsConfig[i].join("");
+        if (str.includes(brackets)) {
+            var str = str.replace(brackets, "");
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+
+    if (str.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
